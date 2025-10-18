0dc5fc003a3bcb714ef6d6734b6e10e86af04391
luisito <luarmoro123@gmail.com>
2025-10-17 19:15:34 -0700

    se inicio el proyecto de movies-app

---
+++ app/_layout.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import "../global.css";
+
+const RootLayout = () => {
+  return (
+    <View>
+      <Text>RootLayout</Text>
+    </View>
+  )
+}
+
+export default RootLayout
